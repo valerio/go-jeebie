c056e5676156394084fef396c0bc2fc9038cc48d
Valerio Pipolo <valep27@gmail.com>
2017-10-02 22:03:23 +0200

    Add simple mmu stub

--- jeebie/memory/mem.go
+++ jeebie/memory/mem.go
@@ -3,3 +3,12 @@ package memory
 // MMU allows access to all memory mapped I/O and data/registers
 type MMU struct {
 }
+
+// TODO: implement stubbed methods
+func (m *MMU) ReadByte(addr uint16) byte {
+	return 0
+}
+
+func (m *MMU) WriteByte(addr uint16, value byte) {
+
+}
