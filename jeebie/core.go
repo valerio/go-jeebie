ee2a267ca6efee33515037785e4f8884c101c24a
Valerio Pipolo <valep27@gmail.com>
2018-01-14 14:50:49 +0100

    move deferred functions outside loop

--- jeebie/core.go
+++ jeebie/core.go
@@ -1,12 +1,13 @@
 package jeebie
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/valep27/go-jeebie/jeebie/cpu"
 	"github.com/valep27/go-jeebie/jeebie/memory"
 	"github.com/valep27/go-jeebie/jeebie/video"
 	"github.com/veandco/go-sdl2/sdl"
-	"io/ioutil"
-	"log"
 )
 
 // Emulator represents the root struct and entry point for running the emulation
@@ -56,6 +57,9 @@ func (e *Emulator) Tick() {
 
 // Run executes the main loop of the emulator
 func (e *Emulator) Run() {
+	defer e.screen.Destroy()
+	defer sdl.Quit()
+
 	for {
 		e.Tick()
 
@@ -64,8 +68,6 @@ func (e *Emulator) Run() {
 			switch t := event.(type) {
 			case *sdl.KeyDownEvent:
 				if t.Keysym.Sym == sdl.K_ESCAPE {
-					defer e.screen.Destroy()
-					defer sdl.Quit()
 					return
 				}
 			case *sdl.KeyUpEvent:
